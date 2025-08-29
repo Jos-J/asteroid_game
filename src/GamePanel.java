57ce02010a05925af677497fcc914a196c2a7b04
JJ <joshua.james918@gmail.com>
2025-08-28 21:39:33 -0500

    expandable skeleton for gamepanel.java

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -4,10 +4,87 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import java.util.Random;
-import java.sound.sampled.*;
+import javax.sound.sampled.*;
 import java.io.File;
 import java.io.IOException;
 
 public class GamePanel extends JPanel implements Runnable {
     
+    // Game thread
+    private Thread gameThread;
+    private boolean running = false;
+
+    // Frame rate
+    private final int FPS = 60;
+    private int FRAME_TIME = 1000 / FPS;
+
+    // Manager & Game objects
+    private  WordManager wordManager;
+    private InputHander inputHander;
+
+    //
+    private Image background;
+
+
+    public GamePanel() {
+        setPreferredSize(new Dimension(800, 600)); // window size
+        setFocusable(true);
+        requestFocus();
+
+
+        // Init managers
+        wordManager = new WordManager();
+        inputHander = new InputHander();
+
+        addKeyListener(inputHander);
+
+        // Load background image
+        background = new ImageIcon("assets/backgoundImage.png").getImage();
+    }
+
+    // start of game loop
+    public void startGame() {
+        if(gameThread == nul || !running) {
+            gameThread = new Thread(this);
+            gameThread.start();
+            running = true;
+        }
+    }
+
+    @Override 
+    pubic void run(){
+        while (running) {
+            long startTime = System.currentTimeMillis();
+
+            update();   // update game logic
+            repaint();  // render
+
+            long endTime = System.currentTimeMillis();
+            long sleepTime = FRAME_TIME - (endTime - startTime);
+
+            if (sleepTime > 0) {
+                try {
+                    Thread.sleep(sleepTime);
+                } catch (InterruptedExecption e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void update() {
+        wordManager.update(); // move asteriods, check collision, etc.
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        // draw background
+        g.drawImage(background, 0, 0, getWidth(), getHeight(), null);
+
+        // draw words/asteroids
+        wordManager.draw(g);
+    }
+
 }
