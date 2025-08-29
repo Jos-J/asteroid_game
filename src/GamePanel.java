1a0e47bcbb2dc046ada0ab6298ba506dd0cd85c8
JJ <joshua.james918@gmail.com>
2025-08-28 22:25:57 -0500

    added test to make sure panel was working

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -17,27 +17,31 @@ public class GamePanel extends JPanel implements Runnable {
     // Frame rate
     private final int FPS = 60;
     private int FRAME_TIME = 1000 / FPS;
-
+/*
     // Manager & Game objects
     private  WordManager wordManager;
     private InputHander inputHander;
+*/
 
     //
     private Image background;
 
+    // test object
+    private int testY = 0;
 
+    
     public GamePanel() {
         setPreferredSize(new Dimension(800, 600)); // window size
         setFocusable(true);
         requestFocus();
 
-
+/*
         // Init managers
         wordManager = new WordManager();
         inputHander = new InputHander();
 
         addKeyListener(inputHander);
-
+*/
         // Load background image
         background = new ImageIcon("assets/backgoundImage.png").getImage();
     }
@@ -71,9 +75,17 @@ public class GamePanel extends JPanel implements Runnable {
             }
         }
     }
-
+/*
     private void update() {
         wordManager.update(); // move asteriods, check collision, etc.
+    }
+*/
+    //update game state
+
+    private void update() {
+        // move test object down
+        testY += 2;
+        if (testY > getHeight()) testY = 0;
     }
 
     @Override
@@ -81,10 +93,25 @@ public class GamePanel extends JPanel implements Runnable {
         super.paintComponent(g);
 
         // draw background
-        g.drawImage(background, 0, 0, getWidth(), getHeight(), null);
+        if (background != null) {
+            g.drawImage(background, 0, 0, getWidth(), getHeight(), null);
+        } else {
+            g.setColor(color.BLACK);
+            g.fillRect(0, 0, getWidth(), getHeight());
+        }
 
+        // draw test object
+        g.setColor(Color.RED);
+        g.fillRect(100, testY, 50, 50);
+
+        // debug text
+        g.setColor(Color.White);
+        g.drawString("GamePanel running...", 10, 20);
+        
+/*
         // draw words/asteroids
         wordManager.draw(g);
+*/
     }
 
 }
