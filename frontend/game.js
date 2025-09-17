9ae6b7f1e8ea1a55bc382e8310ece4db0065798a
JJ <joshua.james918@gmail.com>
2025-09-16 21:43:34 -0500

    added canvas border, js connecting to html, styled buttons, and canvas

---
+++ frontend/game.js
@@ -0,0 +1,7 @@
+var c = document.getElementById("asteroid-game-canvas");
+var ctx = c.getContext("2d");
+ctx.beginPath();
+ctx.stroke();
+ctx.font = "30px Arial";
+ctx.fillStyle = "green";
+ctx.fillText("Hello World", 10, 50);
